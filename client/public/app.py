8fe9316ce091171b2956f37b15b4b002e3ccd51c
Vinayak_Gavariya <vinayakgavariya123@gmail.com>
2023-02-11 18:04:16 +0530

    third commit

--- client/public/app.py
+++ client/public/app.py
@@ -11,29 +11,18 @@ st.subheader('hope we are helpful to you :)')
 excel_file = 'Survey_Results.xlsx'
 sheet_name = 'DATA'
 
-df = pd.read_excel(excel_file,
-                   sheet_name=sheet_name,
-                   usecols='B:D',
-                   header=3)
+df = pd.read_excel(excel_file,sheet_name=sheet_name,usecols='B:D',header=3)
 
-df_participants = pd.read_excel(excel_file,
-                                sheet_name= sheet_name,
-                                usecols='F:G',
-                                header=3)
+df_participants = pd.read_excel(excel_file,sheet_name= sheet_name,usecols='F:G',header=3)
 df_participants.dropna(inplace=True)
 
 # --- STREAMLIT SELECTION
 company = df['Company'].unique().tolist()
 ages = df['Year'].unique().tolist()
 
-age_selection = st.slider('Year:',
-                        min_value= min(ages),
-                        max_value= max(ages),
-                        value=(min(ages),max(ages)))
+age_selection = st.slider('Year:', min_value= min(ages), max_value= max(ages), value=(min(ages),max(ages)))
 
-company_selection = st.multiselect('Company:',
-                                   company,
-                                    default=company)
+company_selection = st.multiselect('Company:', company,default=company)
 
 # --- FILTER DATAFRAME BASED ON SELECTION
 mask = (df['Year'].between(*age_selection)) & (df['Company'].isin(company_selection))
@@ -46,10 +35,6 @@ df_grouped = df_grouped.rename(columns={'Year': 'Placed_Students'})
 df_grouped = df_grouped.reset_index()
 
 # --- PLOT BAR CHART
-bar_chart = px.bar(df_grouped,
-                   x='Job_Role',
-                   y='Placed_Students',
-                   text='Placed_Students',
-                   color_discrete_sequence = ['#F63366']*len(df_grouped),
-                   template= 'plotly_white')
+bar_chart = px.bar(df_grouped, x='Job_Role', y='Placed_Students', text='Placed_Students', color_discrete_sequence = ['#F63366']*len(df_grouped), template= 'plotly_white')
 st.plotly_chart(bar_chart)
+  
