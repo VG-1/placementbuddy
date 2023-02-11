91a66e4e242730b19d9a024f10f91ef077683d6e
Vinayak_Gavariya <vinayakgavariya123@gmail.com>
2023-02-12 02:13:17 +0530

    fourth commit

---
+++ app.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from PIL import Image
+
+
+st.set_page_config(page_title='Survey Results')
+st.button('Log In')
+st.header('heyooo here is shining statisitics of our placements')
+st.subheader('hope we are helpful to you :)')
+
+### --- LOAD DATAFRAME
+excel_file = 'Survey_Results.xlsx'
+sheet_name = 'DATA'
+
+df = pd.read_excel(excel_file,
+                   sheet_name=sheet_name,
+                   usecols='B:D',
+                   header=3)
+
+df_participants = pd.read_excel(excel_file,
+                                sheet_name= sheet_name,
+                                usecols='F:G',
+                                header=3)
+df_participants.dropna(inplace=True)
+
+# --- STREAMLIT SELECTION
+company = df['Company'].unique().tolist()
+ages = df['Year'].unique().tolist()
+
+age_selection = st.slider('Year:',
+                        min_value= min(ages),
+                        max_value= max(ages),
+                        value=(min(ages),max(ages)))
+
+company_selection = st.multiselect('Company:',
+                                   company,
+                                    default=company)
+
+# --- FILTER DATAFRAME BASED ON SELECTION
+mask = (df['Year'].between(*age_selection)) & (df['Company'].isin(company_selection))
+number_of_result = df[mask].shape[0]
+st.markdown(f'*Available Results: {number_of_result}*')
+
+# --- GROUP DATAFRAME AFTER SELECTION
+df_grouped = df[mask].groupby(by=['Job_Role']).count()[['Year']]
+df_grouped = df_grouped.rename(columns={'Year': 'Placed_Students'})
+df_grouped = df_grouped.reset_index()
+
+# --- PLOT BAR CHART
+bar_chart = px.bar(df_grouped,
+                   x='Job_Role',
+                   y='Placed_Students',
+                   text='Placed_Students',
+                   color_discrete_sequence = ['#F63366']*len(df_grouped),
+                   template= 'plotly_white')
+st.plotly_chart(bar_chart)
+
+# --- DISPLAY IMAGE & DATAFRAME
+col1, col2 = st.columns(2)
+image = Image.open('images/survey.jpg')
+col1.image(image,
+        caption='Designed by slidesgo / Freepik',
+        use_column_width=True)
+col2.dataframe(df[mask])
+
+# --- PLOT PIE CHART
+pie_chart = px.pie(df_participants,
+                title='Total No. of Participants',
+                values='Participants',
+                names='Companies')
+
+st.plotly_chart(pie_chart)
---
+++ pages/resources.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as com
+# com.html("""
+
+# <div>
+# <h1>
+# <div class="iframely-embed">
+# <div class="iframely-responsive" style="padding-bottom: 52.5%; padding-top: 120px;"><a href="https://roadmap.sh/frontend" data-iframely-url="//iframely.net/XJ5xa8M"></a></div></div><script async src="//iframely.net/embed.js"></script>
+# </h1>
+# dddhdj
+# <div>
+# <h1>
+# <div class="iframely-embed">
+# <div class="iframely-responsive" style="padding-bottom: 52.5%; padding-top: 120px;"><a href="https://roadmap.sh/backend" data-iframely-url="//iframely.net/XJ5xa8M"></a></div></div><script async src="//iframely.net/embed.js"></script>
+# </h1>
+
+# </div>
+
+# """)
+
+st.header('heyooo we are here to help you throughout the placement procedure')
+st.subheader('please find all the resources and roadmaps here :)')
+url = 'https://roadmap.sh/pdfs/roadmaps/frontend.pdf'
+
+st.markdown(f'''
+<a href={url}><button style="background-color:GreenYellow;">Front-end Roadmap</button></a>
+''',
+unsafe_allow_html=True)
+url = 'https://roadmap.sh/pdfs/roadmaps/backend.pdf'
+
+st.markdown(f'''
+<a href={url}><button style="background-color:GreenYellow;">Back-end Roadmap</button></a>
+''',
+unsafe_allow_html=True)
+url = 'https://roadmap.sh/pdfs/roadmaps/devops.pdf'
+
+st.markdown(f'''
+<a href={url}><button style="background-color:GreenYellow;">DevOps Roadmap</button></a>
+''',
+unsafe_allow_html=True)
+url = 'https://roadmap.sh/pdfs/roadmaps/blockchain.pdf'
+
+st.markdown(f'''
+<a href={url}><button style="background-color:GreenYellow;">Blockchain Roadmap</button></a>
+''',
+unsafe_allow_html=True)
+
+com.html("""
+
+<iframe  src="https://www.youtube.com/embed/nFQ22Wb6Qe8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></h1>
+
+
+""")
+com.html("""
+
+<iframe   src="https://www.youtube.com/embed/nFQ22Wb6Qe8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></h1>
+
+
+""")
+com.html("""
+
+<div style="left: 0; width: 100%; height: 600px; position: relative;"><iframe src="https://9bw5lmx29ue.typeform.com/to/NljNEayv?typeform-embed=oembed&typeform-medium=embed-oembed&format=json&disable-auto-focus=true" style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen allow="encrypted-media;"></iframe></div>
+
+""")
