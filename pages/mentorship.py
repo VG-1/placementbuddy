0b78792e00c52f7a3790f0bc8016a4408e7e4256
Vinayak_Gavariya <vinayakgavariya123@gmail.com>
2023-02-12 20:57:06 +0530

    2nd update

---
+++ pages/mentorship.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as com
+st.header('Meet our mentors who will solve your all doubts related to career and placements')
+
+
+url = 'https://www.linkedin.com/in/arshgoyal/'
+
+st.markdown(f'''
+<a href={url}><button style="background-color:BlueYellow;">Arsh Goyal, Senior SDE @Samsung</button></a>
+
+''',
+unsafe_allow_html=True)
+st.caption('Arsh goyal currently work as a senior software engineer at samsung . He passed out of NIT J. He is a B.Tech Gold Medalist')
+st.caption('You can find him on twitter @ arsh_goyal')
+url = 'https://calendly.com/vinayakgavariya/30-minute-mock-interview'
+
+st.markdown(f'''
+<a href={url}><button style="background-color:GreenYellow;">Book your mentorship session.</button></a>
+
+''',
+unsafe_allow_html=True)
+url = 'https://www.linkedin.com/in/kritarth-mittal/'
+
+st.markdown(f'''
+<a href={url}><button style="background-color:BlueYellow;">Kritarth Mittal, Founder Soshals</button></a>
+
+''',
+unsafe_allow_html=True)
+st.caption('Kritarth is the founder of Inception Wave. He is a software development engineer by profession and a Business Development enthusiast by passion')
+st.caption('You can find him on twitter @ kritarthmittal')
+url = 'https://calendly.com/vinayakgavariya/30-minute-mock-interview'
+
+st.markdown(f'''
+<a href={url}><button style="background-color:GreenYellow;">Book your mentorship session.</button></a>
+
+''',
+unsafe_allow_html=True)
--- pages/test.py
+++ pages/test.py
@@ -2,9 +2,11 @@ import streamlit as st
 import pandas as pd
 import streamlit.components.v1 as com
 
+st.subheader('Take your skill test here ')
 
 
 com.html("""
+
 
 <div class="form">
  <div data-tf-widget="NljNEayv" data-tf-opacity="100" data-tf-inline-on-mobile data-tf-iframe-props="title=Data Analyst" data-tf-transitive-search-params data-tf-auto-focus data-tf-medium="snippet" data-tf-full-screen data-tf-hidden="dataanalyst="></div><script src="//embed.typeform.com/next/embed.js"></script> 
